151a07b577ae79a3a47d5214ffed5d4e3fad13cc
gdelgado <gdelgado@gmail.com>
2022-11-27 19:39:54 -0600

    relase

--- .output/server/chunks/app/client.manifest.mjs
+++ .output/server/chunks/app/client.manifest.mjs
@@ -204,50 +204,50 @@ const client_manifest = {
     "file": "error-500.e60962de.css",
     "resourceType": "style"
   },
-  "node_modules/nuxt/dist/app/entry.css": {
+  "pages/about-us.css": {
     "resourceType": "style",
-    "file": "entry.6c161d8b.css",
-    "src": "node_modules/nuxt/dist/app/entry.css"
-  },
-  "pages/category/[id].css": {
-    "resourceType": "style",
-    "file": "_id_.b4527117.css",
-    "src": "pages/category/[id].css"
+    "file": "about-us.a7ea271d.css",
+    "src": "pages/about-us.css"
   },
   "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
     "resourceType": "style",
     "file": "error-404.18ced855.css",
     "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
   },
-  "pages/about-us.css": {
+  "layouts/default.css": {
     "resourceType": "style",
-    "file": "about-us.a7ea271d.css",
-    "src": "pages/about-us.css"
+    "file": "default.ab72116d.css",
+    "src": "layouts/default.css"
   },
-  "pages/notes.css": {
+  "node_modules/nuxt/dist/app/entry.css": {
     "resourceType": "style",
-    "file": "notes.a0e91c6e.css",
-    "src": "pages/notes.css"
-  },
-  "pages/categories.css": {
-    "resourceType": "style",
-    "file": "categories.9419d875.css",
-    "src": "pages/categories.css"
-  },
-  "pages/note/[id].css": {
-    "resourceType": "style",
-    "file": "_id_.2a790ac7.css",
-    "src": "pages/note/[id].css"
+    "file": "entry.6c161d8b.css",
+    "src": "node_modules/nuxt/dist/app/entry.css"
   },
   "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
     "resourceType": "style",
     "file": "error-500.e60962de.css",
     "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
   },
-  "layouts/default.css": {
+  "pages/notes.css": {
     "resourceType": "style",
-    "file": "default.ab72116d.css",
-    "src": "layouts/default.css"
+    "file": "notes.a0e91c6e.css",
+    "src": "pages/notes.css"
+  },
+  "pages/note/[id].css": {
+    "resourceType": "style",
+    "file": "_id_.2a790ac7.css",
+    "src": "pages/note/[id].css"
+  },
+  "pages/categories.css": {
+    "resourceType": "style",
+    "file": "categories.9419d875.css",
+    "src": "pages/categories.css"
+  },
+  "pages/category/[id].css": {
+    "resourceType": "style",
+    "file": "_id_.b4527117.css",
+    "src": "pages/category/[id].css"
   },
   "entryCSS": {
     "file": "",
